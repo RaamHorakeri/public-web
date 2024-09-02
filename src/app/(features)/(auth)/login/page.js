28cc475afb0ba7427bc15f0cce657627d74731bc
RavindraBabu-Enfec <ravindra.polnedi@enfec.com>
2024-09-02 16:49:17 +0530

    feat: integrated api's for signup and login pages

--- src/app/(features)/(auth)/login/page.js
+++ src/app/(features)/(auth)/login/page.js
@@ -1,61 +1,196 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
+import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
 
-import Image from 'next/image';
-import Input from '@/components/Input';
-
-
+const defaultErrorMsg = {
+  email: null,
+  password: null,
+};
 const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(defaultErrorMsg);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setErrorMessage(defaultErrorMsg);
+    let errors = {};
+    if (!email) {
+      errors.email = "email should not be empty.";
+    }
+
+    if (!password) {
+      errors.password = "password should not be empty.";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setErrorMessage(errors);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        "http://localhost:3050/api/v1/account/authenticate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Login successful:", data);
+        router.push("/");
+      } else {
+        const errorData = await response.json();
+
+        setErrorMessage(errorData.message || "login failed. Please try again.");
+        console.error("Login failed:", data.error);
+      }
+    } catch (error) {
+      setErrorMessage("An error occurred. Please try again later.");
+      console.error("Error:", error);
+    }
+  };
+
+  const handleGoogleLogin = () => {
+    // window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:3000/&response_type=code&scope=openid%20email%20profile`;
+  };
+
+  const handleGitHubLogin = () => {
+    // window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}&redirect_uri=http://localhost:3000/&scope=user:email`;
+  };
+
   return (
-<section className="flex justify-center items-center h-screen mt-12 mb-20 ">
-  <div className="w-[680px] h-auto bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
-    <h1 className="w-[520px] h-[35px] mx-auto font-roboto font-bold text-m leading-[35.2px] text-center text-[#21272A]">
-      Login
-    </h1>
-    <form className="mt-[40px] flex flex-col items-center gap-1">
-     
-      <label className="w-[520px] text-left font-roboto font-normal text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
-        User Name / Email
-      </label>
-      <Input type='text'  />
-     
+    <section className="flex justify-center items-center h-screen mt-12 mb-20">
+      <div className="w-[680px] h-auto bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
+        <h1 className="w-[520px] h-[35px] mx-auto font-roboto font-bold text-m leading-[35.2px] text-center text-[#21272A]">
+          Login
+        </h1>
+        <form
+          className="mt-[40px] flex flex-col items-center gap-1"
+          onSubmit={handleSubmit}
+        >
+          <label className="w-[520px] text-left font-roboto font-normal text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
+            User Name / Email
+          </label>
+          <Input
+            type="text"
+            value={email}
+            onChange={(e) => {
+              const email = e.target.value;
+              setEmail(email);
+              if (errorMessage.email && email.length > 0) {
+                setErrorMessage((error) => {
+                  return { ...error, email: null };
+                });
+              }
+            }}
+          />
+          {errorMessage.email && (
+            <div className="w-[520px] mt-1  text-red-500 text-[12px]">
+              *{errorMessage.email}
+            </div>
+          )}
 
-     
-      <label className="w-[520px] text-left font-roboto font-normal text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
-        Password
-      </label>
-      <Input type='password'  />
+          <label className="w-[520px] text-left font-roboto font-normal text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
+            Password
+          </label>
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              const password = e.target.value;
+              setPassword(password);
+              if (errorMessage.password && password.length > 0) {
+                setErrorMessage((error) => {
+                  return { ...error, password: null };
+                });
+              }
+            }}
+          />
 
+          {errorMessage.password && (
+            <div className="w-[520px] mt-1  text-red-500 text-[12px]">
+              *{errorMessage.password}
+            </div>
+          )}
+          <div className="w-[520px] h-[20px] flex justify-between items-center mb-[24px]">
+            <div className="flex items-center">
+              <Input
+                type="checkbox"
+                id="rememberMe"
+                className="mr-[8px]"
+                width="w-[15px]"
+                height="h-[15px]"
+              />
 
-      
-      <div className="w-[520px] h-[20px] flex justify-between items-center mb-[24px]">
-        <div className="flex items-center">
-        <Input type='checkbox' id="rememberMe" className='mr-[8px]' width='w-[15px]' height='h-[15px]' />
-          <label htmlFor="rememberMe" className="font-roboto text-[14px] leading-[16.8px] font-normal text-[#B7B7B7]">Remember me</label>
-        </div>
-        <Link href="/forgotpassword" className="font-roboto text-[14px] leading-[16.8px] font-normal text-primary">Forgot password?</Link>
+              <label
+                htmlFor="rememberMe"
+                className="font-roboto text-[14px] leading-[16.8px] font-normal text-[#B7B7B7]"
+              >
+                Remember me
+              </label>
+            </div>
+            <Link
+              href="/forgotpassword"
+              className="font-roboto text-[14px] leading-[16.8px] font-normal text-primary"
+            >
+              Forgot password?
+            </Link>
+          </div>
+
+          <p className="w-[520px] text-center text-xs font-roboto font-normal leading-[19.2px] text-[#CFCFCF] mb-[24px]">
+            or
+          </p>
+
+          <div className="w-[520px] flex justify-between mb-[24px]">
+            <button
+              type="button"
+              onClick={handleGoogleLogin}
+              className="flex items-center justify-center space-x-2 w-[244px] h-[50px] rounded-3xl p-3 bg-white border border-[#B7B7B7]"
+            >
+              <Image
+                src="/images/google.png"
+                width={20}
+                height={20}
+                alt="googleLogo"
+              />
+              <span className="text-[#0A0A0B] font-roboto text-xs leading-[19.2px] font-normal">
+                Sign in with Google
+              </span>
+            </button>
+            <button
+              type="button"
+              onClick={handleGitHubLogin}
+              className="flex items-center justify-center space-x-2 w-[244px] h-[50px] rounded-3xl p-3 bg-white border border-[#B7B7B7]"
+            >
+              <Image
+                src="/images/Github.png"
+                width={20}
+                height={20}
+                alt="githubLogo"
+              />
+              <span className="text-[#0A0A0B] font-roboto text-xs leading-[19.2px] font-normal">
+                Sign in with GitHub
+              </span>
+            </button>
+          </div>
+
+          <button className="w-[520px] h-[48px] bg-primary text-secondary-100 font-roboto font-medium text-xs leading-[16px] mt-6">
+            Login
+          </button>
+        </form>
       </div>
+    </section>
+  );
+};
 
-      
-      <p className="w-[520px] text-center text-xs font-roboto font-normal leading-[19.2px] text-[#CFCFCF] mb-[24px]">or</p>
-
-      
-      <div className="w-[520px] flex justify-between mb-[24px]">
-        <button className=" flex items-center justify-center space-x-2 w-[244px] h-[50px] rounded-3xl p-3 bg-white border border-[#B7B7B7]  "> <Image src="/images/google.png" width={20} height={20} alt='googleLogo' /> <span className="text-[#0A0A0B]  font-roboto text-xs leading-[19.2px] font-normal ">Sign in with Google</span> </button>
-        <button className=" flex items-center justify-center space-x-2 w-[244px] h-[50px] rounded-3xl p-3 bg-white border border-[#B7B7B7]  "> <Image src="/images/Github.png" width={20} height={20} alt='googleLogo' /> <span className="text-[#0A0A0B]  font-roboto text-xs leading-[19.2px] font-normal">Sign in with GitHub</span> </button>
-      </div>
-
-     
-      <button className="w-[520px] h-[48px] bg-primary text-secondary-100 font-roboto font-medium text-xs leading-[16px] mt-6">
-
-        Login
-      </button>
-    </form>
-  </div>
-</section>
-
-
-  )
-}
-
-export default Page
+export default Page;
--- src/components/TwoFa.js
+++ src/components/TwoFa.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Input from "./Input";
-import Link from "next/link";
+import { useState } from "react";
 
-const TwoFA = () => {
+const TwoFA = ({ onSubmitTwoFa }) => {
+  const [code, setCode] = useState("");
+  const [error, setError] = useState("");
+
+  const onChangeHandler = (e) => {
+    const code = e.target.value;
+    setCode(code);
+    if (error && code.length === 6) {
+      setError("");
+    }
+  };
+
+  const onSubmitHandler = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (code.length !== 6) {
+      setError("Code must be exactly 6 characters long.");
+    } else {
+      onSubmitTwoFa(code);
+    }
+  };
   return (
-    <section className="flex justify-center items-center h-screen mb-20 ">
-      <div className="w-[680px] h-auto bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
+    <section className="flex justify-center items-center">
+      <div className=" bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
         <h1 className="w-[520px] h-[35px] mx-auto font-roboto font-bold text-m leading-[35.2px] text-center text-[#21272A]">
           2-Factor Authentication
         </h1>
@@ -14,17 +34,22 @@ const TwoFA = () => {
             Enter 6-digit Code that you recieved in your mail
           </label>
 
-          <Input type="text" />
-
-          <div className="w-[520px] flex justify-end items-center mb-[24px]">
+          <Input type="text" value={code} onChange={onChangeHandler} />
+          {error && (
+            <p className="text-red-600 text-[12px] mt-1 w-[520px] text-left">
+              {error}
+            </p>
+          )}
+          <div className="w-[520px] flex justify-end items-center ">
             <p className="font-roboto text-[14px] text-primary">5:05 min</p>
           </div>
 
-          <Link href="/resetpassword">
-            <button className="w-[520px] h-[48px] bg-primary text-secondary-100 font-medium text-xs leading-[16px] mt-6">
-              Submit
-            </button>
-          </Link>
+          <button
+            onClick={onSubmitHandler}
+            className="w-[520px] h-[48px] bg-primary text-secondary-100 font-medium text-xs leading-[16px] "
+          >
+            Submit
+          </button>
         </form>
       </div>
     </section>
