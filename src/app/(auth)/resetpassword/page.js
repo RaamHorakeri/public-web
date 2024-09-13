09cf460554bb69aa503190f841810e6402e6e0d7
RavindraBabu-Enfec <ravindra.polnedi@enfec.com>
2024-09-13 11:00:06 +0530

    fix: moved auth directory from features directory to app directory

--- src/app/(auth)/resetpassword/page.js
+++ src/app/(auth)/resetpassword/page.js
@@ -1,15 +1,20 @@
-'use client'
-import React, { useState } from 'react';
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-import Link from 'next/link';
-import Input from '@/components/Input';
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import Link from "next/link";
+import Input from "@/components/Input";
 
 const Page = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const handleSubmit = (event) => {
-      event.preventDefault();
-      setOpen(true);
+    event.preventDefault();
+    setOpen(true);
   };
 
   return (
@@ -18,17 +23,19 @@ const Page = () => {
         <h1 className="w-[520px] h-[35px] mx-auto font-roboto font-bold text-m leading-[35.2px] text-center text-[#21272A]">
           Reset Password
         </h1>
-        <form onSubmit={handleSubmit} className="mt-[40px] flex flex-col items-center gap-1">
+        <form
+          onSubmit={handleSubmit}
+          className="mt-[40px] flex flex-col items-center gap-1"
+        >
           <label className="w-[520px] text-left font-roboto font-medium text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
             Create New Password
           </label>
-          <Input type='password'  />
+          <Input type="password" />
 
           <label className="w-[520px] text-left font-roboto font-medium text-[14px] mb-2 leading-[19.6px] text-[#21272A]">
             Confirm Password
           </label>
-                <Input type='password'  />
-
+          <Input type="password" />
 
           <button
             type="submit"
@@ -40,8 +47,6 @@ const Page = () => {
       </div>
 
       {open && (
-
-
         <Dialog open={open} onClose={setOpen} className="relative z-10">
           <DialogBackdrop
             transition
@@ -56,34 +61,29 @@ const Page = () => {
               >
                 <div className="bg-white flex items-center justify-center ">
                   <div className="flex mt-7 items-center justify-center ">
-
                     <div className="mt-3 text-center flex flex-col justify-center items-center  ">
-
                       <div className="flex items-center justify-center h-[35px] w-[697px] text-[#21272A] font-roboto font-bold leading-[35.2px] ">
-                        <h1 className='mb-5'>Password is changed.Please login here</h1>
+                        <h1 className="mb-5">
+                          Password is changed.Please login here
+                        </h1>
                       </div>
                       <div className="flex items-center justify-center px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
                         <Link
-                        href='login'
+                          href="login"
                           type="button"
                           onClick={() => setOpen(false)}
                           className="flex w-[82px] h-[48px] rounded-md bg-white leading-[24px] items-center justify-center py-2 text-s font-medium text-primary border border-primary"
                         >
                           Login
                         </Link>
-
                       </div>
                     </div>
                   </div>
                 </div>
-
               </DialogPanel>
             </div>
           </div>
         </Dialog>
-
-
-
       )}
     </section>
   );
