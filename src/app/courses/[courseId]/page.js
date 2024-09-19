c6d80ce264e3b2a85d09270089a0f7be6bf5b416
RavindraBabu-Enfec <ravindra.polnedi@enfec.com>
2024-09-19 09:58:26 +0530

    feat: updated design of course detail page

--- src/app/courses/[courseId]/page.js
+++ src/app/courses/[courseId]/page.js
@@ -1,85 +1,202 @@
-import React from "react";
-
-const Page = () => {
+import Image from "next/image";
+export default function Page() {
   return (
-    <div>
-      <section className="bg-[#F4F1FA] py-0 px-[87px] h-[320px] flex flex-col items-center justify-center gap-6">
-        <h1 className="font-roboto font-semibold text-[32px] leading-[38.4px] text-center text-[#7C56CF]">
-          Java Software Engineer
-        </h1>
-        <p className="font-roboto font-normal text-[14px] leading-[16.8px] text-center">
-          Home/Courses/Java Software Engineer
-        </p>
-      </section>
-      <section className="w-full h-auto bg-white p-[120px_87px] gap-[60px]">
-        <div className="flex bg-white  rounded-md w-full h-auto p-[30px]">
-          <div className="flex-1 flex flex-col gap-[40px]">
-            <div>
-              <h1 className="w-[813px] h-[84px] text-left font-roboto font-semibold text-[30px] text-[#3B2174] leading-[42px]">
-                Java Software Engineering Series: Master Core Concepts and
-                Advance Your Skills
+    <>
+      <div className="bg-black min-h-[100vh] w-full text-white font-sans p-10 flex justify-between">
+        <div className="h-[100%] flex flex-col justify-center items-center">
+          <div className="w-[2px] h-[380px] bg-[#E151FF] mt-16 "></div>
+          <div className="bg-[#E151FF] my-4 flex justify-around">
+            <span className="material-symbols-outlined">code</span>
+          </div>
+          <div className="w-[2px] h-[170px] bg-gradient-to-b from-[#E151FF] via-[#E151FF] to-[#333]  animate2"></div>
+        </div>
+        <div className="w-[95%]">
+          <div className="flex justify-between">
+            <div className="w-[60%] flex flex-col justify-evenly">
+              <h1 className=" font-[800] text-[60px]">
+                Become a
+                <span className="ml-3 text-transparent bg-clip-text bg-gradient-to-r from-[#714DFF] via-[#E151FF] to-[#FFF759]">
+                  Java Software Engineer
+                </span>
               </h1>
-              <div className="flex justify-center mt-[20px]">
-                <button className="bg-white text-[#7C56CF] py-2 px-6  border  border-[#7C56CF] w-[120px] h-[48px] rounded-[5px]">
-                  Get Help
+              <div className="font-[400] text-[18px] w-[48%]">
+                Build customer relationships with CRM tools that can accommodate
+                all needs and are also integrated
+              </div>
+              <div className="flex text-[16px] font-[600]">
+                <button className="border-[2px] rounded-lg px-4 p-2 w-[20%]">
+                  Enroll Now
+                </button>
+                <button className="border rounded-lg px-4 p-2 ml-4 w-[20%] bg-white text-black">
+                  Get Started
                 </button>
               </div>
+              <div className="flex  justify-start items-center gap-2">
+                <span className="font-[400] text-[18px] ">Excellent</span>
+                <span className="font-[700] text-[18px]">4.7 out of 5</span>
+                <span
+                  className="material-symbols-outlined"
+                  style={{ color: "#12FBAA" }}
+                >
+                  star
+                </span>
+                <span className="font-[700] text-[18px]">Trustpilot</span>
+              </div>
             </div>
-
-            <div className="w-full h-auto">
-              <hr
-                className="bg-[#E5DDF5] border border-1 border-[#E5DDF5] mb-10"
-                style={{ width: "813px" }}
+            <div className="mr-10">
+              <Image
+                src="/images/connect.png"
+                alt="connect image"
+                width={450}
+                height={100}
               />
-              <h3 className="w-[813px] h-[24px] text-left font-roboto font-semibold text-[20px]  text-[#3B2174]  leading-[24px]">
-                Details About the Course
-              </h3>
-              <p className="w-[813px] h-[120px] text-left font-roboto font-regular text-[16px] text-[#868686] leading-[30px] mt-[20px]">
-                The Java Software Engineering program is structured to guide you
-                from a beginner level to a professional level. You&apos;ll start
-                with basic Java programming concepts and progress to advanced
-                topics and real-world applications. Each module includes
-                hands-on projects, quizzes, and assessments to reinforce your
-                learning and track your progress.
-              </p>
             </div>
           </div>
-
-          <div className="w-[396px] h-auto flex flex-col gap-[26px] ml-[30px]">
-            <div className="flex flex-col gap-[26px] bg-white p-[15px]  rounded-md border  border-[#7C56CF]  ">
-              <h4 className="bg-[#F2EEFA] p-[10px] text-center font-roboto font-semibold text-[20px] leading-[43px]">
-                Java Software Engineer
-              </h4>
-
-              <p className="flex items-center justify-center text-[#FF6F61] gap-[8px]">
-                <svg
-                  className="w-[20px] h-[20px] text-[#FF6F61]"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M12 8v4l3 3M12 6a9 9 0 100 18 9 9 0 000-18z"
-                  />
-                </svg>
-                <span className="text-center font-roboto font-regular text-[16px] leading-[30px]">
-                  5 days left to start
-                </span>
-              </p>
-
-              <button className=" h-auto bg-[#7C56CF] text-[#FAFAFA] font-medium text-[20px] text-center leading-[24px] p-[12px] mt-[20px] rounded-[5px]">
-                Enroll
-              </button>
+          <div className="text-center flex justify-center mt-8">
+            <div className=" font-[700] text-[24px] text-center  w-[50%]">
+              If you learn Java, you can develop a wide range of applications
+              due to Java&apos;s versatility and robustness.
+            </div>
+          </div>
+          <div className="flex justify-around mt-8 font-[700] text-[16px]">
+            <div>Web Applications</div>
+            <div>Mobile Applications</div>
+            <div>Desktop Applications</div>
+            <div>IoT Applications</div>
+            <div>Game development</div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-black min-h-[100vh] w-full text-white font-sans p-10 ">
+        <div className="flex justify-between">
+          <div className="h-[100%] flex flex-col justify-center items-center">
+            <div className="bg-green-600 mb-4 grid place-content-center ">
+              <span className="material-symbols-outlined">work</span>
+            </div>
+            <div className="w-[2px] h-[240px]  bg-green-400 bg-gradient-to-b from-[#05f649] via-[#05f649] to-[#333] animate2"></div>
+          </div>
+          <div className="w-[95%]">
+            <div className="flex">
+              <div className="w-[60%] flex flex-col justify-evenly">
+                <div className="font-[700] text-[24px]">Productivity</div>
+                <h1 className=" font-[700] text-[48px] w-[70%] leading-tight">
+                  <span className=" mr-3 text-transparent bg-clip-text bg-green-400">
+                    Engage with
+                  </span>
+                  your customers instantly
+                </h1>
+                <div className="flex gap-10 p-2 mt-10">
+                  <div className="border-b-2 border-green-400 w-[160px] p-2">
+                    Assesments
+                  </div>
+                  <div className="border-b-2 border-gray-400 w-[160px] p-2">
+                    Practices
+                  </div>
+                  <div className="border-b-2 border-gray-400 w-[160px] p-2">
+                    Interviews
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
-  );
-};
+        <div className="flex items-center animate ">
+          <Image
+            src="/images/CodeFrame1.png"
+            alt="connect image"
+            width={500}
+            height={100}
+            className="w-[45%]"
+          />
 
-export default Page;
+          <Image
+            src="/images/CodeFrame2.png"
+            alt="connect image"
+            width={500}
+            height={100}
+            className="w-[45%] ml-[60px]"
+          />
+        </div>
+      </div>
+      <div className="bg-black min-h-[100vh] w-full text-white font-sans p-10">
+        <div className="flex justify-between">
+          <div className="h-[100%] flex flex-col justify-center items-center">
+            <div className="bg-[#e44b2c] mb-4 grid place-content-center">
+              <span className="material-symbols-outlined">favorite</span>
+            </div>
+            <div className="w-[2px] h-[340px]  bg-gradient-to-b from-[#ffffff]  via-[#CE583F] to-[#333] animate2"></div>
+          </div>
+          <div className="w-[95%]">
+            <div className="flex">
+              <div className=" flex flex-col justify-evenly">
+                <div className="font-[700] text-[24px]">Productivity</div>
+                <h1 className=" font-[700] text-[48px] w-[70%] leading-tight">
+                  <span className=" mr-3 text-transparent bg-clip-text bg-[#CE583F]">
+                    Java Software
+                  </span>
+                  Engineer
+                </h1>
+                <div className="font-[700] text-[18px] mb-3 mt-5 text-[#D3D3D3]">
+                  We care for your technology so you can care for your Career
+                </div>
+                <div className=" mt-10 font-[700] text-[18px] w-[83%]">
+                  Scale has pioneered in the data labeling industry by combining
+                  AI-based techniques with human-in-the-loop, delivering labeled
+                  data at unprecedented quality, scalability, and efficiency.
+                </div>
+                <div className=" font-[700] text-[18px] w-[83%]">
+                  Scale has pioneered in the data labeling industry by combining
+                  AI-based techniques with human-in-the-loop, delivering labeled
+                  data at unprecedented quality, scalability, and efficiency.
+                </div>
+                <div className="  font-[700] text-[18px] w-[83%]">
+                  Scale has pioneered in the data labeling industry by combining
+                  AI-based techniques with human-in-the-loop, delivering labeled
+                  data at unprecedented quality, scalability, and efficiency.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center animate">
+          <Image
+            src="/images/CodeFrame1.png"
+            alt="connect image"
+            width={500}
+            height={100}
+            className="w-[45%]"
+          />
+
+          <Image
+            src="/images/CodeFrame2.png"
+            alt="connect image"
+            width={500}
+            height={100}
+            className="w-[45%] ml-[60px]"
+          />
+        </div>
+        <div className="flex flex-col gap-4 items-center mt-11 min-h-[500px] justify-center">
+          <h1 className=" font-[700] text-[48px]">
+            Grow
+            <span className="ml-3 mr-3 text-transparent bg-clip-text bg-gradient-to-r from-[#714DFF] via-[#E151FF] to-[#FFF759]">
+              Better With Us
+            </span>
+            Today
+          </h1>
+          <div className="font-[400] text-[18px] w-[44%] text-center">
+            Improve the quality of customer relationships,generate leads,and
+            maximize product sales with just one integrated CRMtool
+          </div>
+          <div className="flex text-[16px] font-[600]">
+            <button className="border-[2px] rounded-lg px-8 p-2 min-w-[140px]">
+              Get Help
+            </button>
+            <button className="border rounded-lg px-8 p-2 ml-4 min-w-[140px] bg-white text-black">
+              Enroll
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
