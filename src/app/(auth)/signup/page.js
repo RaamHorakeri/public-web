b0e919ccdb112df1cb347d67e886ecc205a2fb1e
Rajuenfec <butchiraju.jeelakarra@enfec.com>
2024-09-24 12:34:06 +0530

    fix: updated the signup design

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -24,6 +24,9 @@ const Page = () => {
   const [activationId, setActivationId] = useState("");
   const [loading, setLoading] = useState(false);
 
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     setErrorMessage(defaultErrorMsg);
@@ -102,8 +105,213 @@ const Page = () => {
   };
 
   return (
-    <section className="flex justify-center items-center h-screen mt-12 mb-20 ">
-      <div className="w-[680px] h-auto bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
+    <section className=" bg-[#ffffff] flex h-[1024px] items-center justify-center p-[80px] gap-4 ">
+      <div className="flex flex-col justify-between w-[44%] h-[100%]">
+        <div className=" bg-[#F3F3F3] rounded-[20px] p-[32px] gap-[50px] h-[80%] flex flex-col ">
+          <p className=" text-[18px] font-normal leading-[24.55px] text-[#777777] ">
+            Sign up now to unlock a world of lettarning opportunities and take
+            the first step towards becoming a software engineer.
+          </p>
+          <form className="space-y-4">
+            <div>
+              <label className="block text-[16px] font-normal leading-[21.82px] text-[#1C1C1C] mb-2">
+                Username
+              </label>
+              <input
+                type="text"
+                placeholder="Enter your name"
+                className="w-full bg-[#ffffff] rounded-[22px] px-[16px] py-[12px] h-[50px] outline-none "
+              />
+            </div>
+            <div>
+              <label className="block text-[16px] font-normal leading-[21.82px] text-[#1C1C1C] mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full bg-[#ffffff] rounded-[22px] px-[16px] py-[12px] h-[50px] outline-none"
+              />
+            </div>
+            {/* <div className="flex space-x-4">
+              <div className="w-1/2">
+                <label className="block text-[16px] font-normal leading-[21.82px] text-[#1C1C1C] mb-2">
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={passwordVisible ? "text" : "password"}
+                    placeholder="Password"
+                    className="w-full bg-[#ffffff] rounded-[22px] px-[16px] py-[12px] h-[50px] outline-none"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setPasswordVisible(!passwordVisible)}
+                    className="absolute right-3 top-1/3 text-gray-400"
+                  >
+                    {passwordVisible ? (
+                      ""
+                    ) : (
+                      <Image src="/images/eye.svg" width={19} height={13} />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <div className="w-1/2">
+                <label className="block text-[16px] font-normal leading-[21.82px] text-[#1C1C1C] mb-2">
+                  Confirm Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={confirmPasswordVisible ? "text" : "password"}
+                    placeholder="Confirm Password"
+                    className="w-full bg-[#ffffff] rounded-[22px] px-[16px] py-[12px] h-[50px] outline-none"
+                  />
+                  <button
+                    type="button"
+                    onClick={() =>
+                      setConfirmPasswordVisible(!confirmPasswordVisible)
+                    }
+                    className="absolute right-3 top-1/3 text-gray-400"
+                  >
+                    {confirmPasswordVisible ? (
+                      "🙈"
+                    ) : (
+                      <Image src="/images/eye.svg" width={19} height={13} />
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div> */}
+
+            <button className="w-full p-[10px] bg-[#1C1C1C] text-white rounded-[22px] text-[18px] font-bold leading-[24.55px] ">
+              Register
+            </button>
+
+            <div className="flex items-center justify-between my-4">
+              <hr className="w-full border-[#CFCFCF]" />
+              <span className="mx-2 text-[#A4A4A4] text-[16px] font-normal leading-[19.2px]">
+                or
+              </span>
+              <hr className="w-full border-[#CFCFCF]" />
+            </div>
+
+            <div className="flex space-x-4">
+              <button className="w-1/2 p-3 border border-[#A4A4A4] flex items-center justify-center rounded-[22px] text-[18px] leading-[24.55px] font-normal">
+                <Image
+                  src="/images/google.png"
+                  width={20}
+                  height={20}
+                  alt="googleLogo"
+                  className="mr-2"
+                />
+                Sign In with Google
+              </button>
+              <button className="w-1/2 p-3 border border-[#A4A4A4] flex items-center justify-center rounded-[22px] text-[18px] leading-[24.55px] font-normal ">
+                <Image
+                  src="/images/Github.png"
+                  width={20}
+                  height={20}
+                  alt="githubLogo"
+                  className="mr-2"
+                />
+                Sign In with Github
+              </button>
+            </div>
+
+            <p className="text-center mt-4 text-[18px] leading-[24.55px] font-normal text-[#4A4A4A] ">
+              Already have an account?{" "}
+              <Link href="/login" className="text-[#1C1C1C]">
+                Login Here
+              </Link>
+            </p>
+          </form>
+        </div>
+        <div className=" bg-[#F3F3F3] flex items-center justify-between rounded-[20px] h-[15%]  p-[20px]">
+          <div className="flex items-center">
+            <div className="flex relative mr-10 w-[75px] h-[45px]">
+              <Image
+                src="/images/ellipse6.svg"
+                alt="userIcon"
+                width={45}
+                height={45}
+                className="absolute left-0 rounded-full z-10" // First image
+              />
+              <Image
+                src="/images/ellipse7.svg"
+                alt="userIcon"
+                width={45}
+                height={45}
+                className="absolute left-[25px] rounded-full z-20" // Second image overlaps 1st by 50%
+              />
+              <Image
+                src="/images/ellipse6.svg"
+                alt="userIcon"
+                width={45}
+                height={45}
+                className="absolute left-[50px] rounded-full z-30" // Third image overlaps 2nd by 50%
+              />
+            </div>
+
+            <div>
+              <p className="text-[#262626] text-[20px] leading-[27.28px] font-bold">
+                Join with 20k+ Users!
+              </p>
+              <p className="text-[#B7B7B7] text-[14px] leading-[19.1px] font-normal">
+                Let&apos;s see our happy customer
+              </p>
+            </div>
+          </div>
+          <button className="p-3 rounded-full">
+            <Image
+              src="/images/topArrowRigth.svg"
+              alt="topRightArrow"
+              width={48}
+              height={48}
+            />
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-between w-[44%] h-[100%] bg-[url('/images/loginBg.png')] bg-cover bg-center rounded-[20px] p-[28px] ">
+        <p className=" text-[#ffffff] text-[36px] leading-[42px] font-bold ">
+          Al Revolutionizing the way we create, render, and experience content.
+        </p>
+        <div className="flex flex-col justify-between bg-[#8967D4A3] w-[100%] h-[200px] p-[30px] rounded-[20px] ">
+          <div className="  flex justify-between items-center  ">
+            <div className="flex">
+              <Image
+                src="/images/Ellipse8.svg"
+                alt="circleIcon"
+                width={55}
+                height={55}
+              />
+              <p className="-ml-5 border-[2px] border-[#fafafa]  text-center py-[16px] rounded-[20px] w-[170px] text-[16px] font-normal leading-[21.82px] text-[#fafafa] ">
+                Creating
+              </p>
+            </div>
+            <div className=" flex items-center justify-center ">
+              <Image
+                src="/images/topArrowleft.svg"
+                alt="topRightArrow"
+                width={55}
+                height={55}
+              />
+              <Image
+                src="/images/topArrowRigth.svg"
+                alt="topRightArrow"
+                width={55}
+                height={55}
+              />
+            </div>
+          </div>
+
+          <p className=" text-[#FAFAFA] font-normal text-[14px] leading-[19.1px] ">
+            Create design brief with Al voice command to make awesome <br />
+            3d images that suits your needs.
+          </p>
+        </div>
+      </div>
+      {/* <div className="w-[680px] h-auto bg-[#F3F3F3] p-[80px] gap-[24px] border border-secondary-100">
         <h1 className="w-[520px] h-[35px] mx-auto font-roboto font-bold text-m leading-[35.2px] text-center text-[#21272A]">
           Register
         </h1>
@@ -241,9 +449,9 @@ const Page = () => {
             {loading ? <Spinner /> : "Register"}
           </button>
         </form>
-      </div>
+      </div> */}
 
-      {openTwoFa && (
+      {/* {openTwoFa && (
         <Dialog
           open={openTwoFa}
           onClose={setOpenTwoFa}
@@ -315,7 +523,7 @@ const Page = () => {
             </div>
           </div>
         </Dialog>
-      )}
+      )} */}
     </section>
   );
 };
