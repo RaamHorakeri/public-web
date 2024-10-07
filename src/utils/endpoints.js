4c20d720071d46738f984e6fde17e8a08aa361cd
RavindraBabu-Enfec <ravindra.polnedi@enfec.com>
2024-10-07 10:47:24 +0530

    fix: url updated

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,3 +1,4 @@
-export const COMMUNITY_URL = "https://community-staging.enfec.cloud/community";
+export const COMMUNITY_URL =
+  "https://community-staging.enfec.cloud/api/v1/community";
 export const AUTH_TOKEN = "test";
 export const AUTH_URL = "https://bifrost.enfec.cloud/api/v1/account";
